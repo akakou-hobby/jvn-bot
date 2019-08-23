8c8b494cd8622095015932f358a35ce0cff3fe61
akakou <s16003@tokyo.kosen-ac.jp>
2019-08-23 11:36:26 +0900

    RSSフィードを取得

--- src/admonitor.py
+++ src/admonitor.py
@@ -9,20 +9,38 @@ __version__ = '0.1'
 __date__    = '2018/08/21'
 
 
-import requests
 import json
 
+import requests
+import feedparser
+
 import settings
+
+
+URL = 'https://jvndb.jvn.jp/ja/rss/jvndb_new.rdf'
 
 
 def send_webhook(data):
     '''WEB HOOKを送信する'''
     requests.post(settings.WEB_HOOK_URL, data=json.dumps(data))
 
+def get_rss():
+    return feedparser.parse(URL)
+
 
 if __name__=='__main__':
-    send_webhook({
-        'text': u'Hello, World!',
-        'link_names': 1
-    })
+    # send_webhook({
+    #     'text': u'Hello, World!',
+    #     'link_names': 1
+    # })
 
+    rss = get_rss()
+    index = 0
+    print(rss['entries'][index]['title'])
+
+    print(rss['entries'][index]['sec_cpe']['vendor'])
+    print(rss['entries'][index]['sec_cpe']['product'])
+    print(rss['entries'][index]['sec_cvss']['severity'])
+    print(rss['entries'][index]['sec_cvss']['score'])
+    print(rss['entries'][index]['link'])
+    print(rss['entries'][index]['summary'])
