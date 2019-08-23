259b26f7c17810876995cc299bc9ff8c76eb537f
akakou <s16003@tokyo.kosen-ac.jp>
2019-08-23 16:42:39 +0900

    条件を扱うモデルとそれを用いたチェック部の作成

---
+++ src/model.py
@@ -0,0 +1,59 @@
+import sys
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, Text, Float, DateTime
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+ENGINE = create_engine('sqlite:///db.sqlite3', echo=True)
+Base = declarative_base()
+
+session = scoped_session(
+    sessionmaker(
+        bind = ENGINE,
+        autocommit=False,
+    )
+)
+
+
+class Condition(Base):
+    '''デバイスの設定情報'''
+    __tablename__ = 'conditions'
+
+    _id = Column('id', Integer, primary_key = True)
+    channel = Column('channel', Text)
+    vendor = Column('vendor', Text)
+    product = Column('product', Text)
+    cvss = Column('cvss', Float)
+
+    def hit(self, vuln):
+        is_same_vendor = True
+        is_same_product = True
+        is_cvss_higher = True
+
+        if self.vendor:
+            is_same_vendor = self.vendor == vuln.vendor
+
+        if self.product:
+            is_same_product = self.product == vuln.product
+    
+        if self.cvss:
+            is_cvss_higher = self.cvss <= vuln.cvss
+        
+        return is_same_vendor \
+            and is_same_product \
+            and is_cvss_higher
+
+
+Base.metadata.create_all(ENGINE)
+
+
+if __name__=='__main__':
+    condition = Condition()
+    condition.channel = 'general'
+    condition.vendor = ''
+    condition.product = ''
+    condition.cvss = 9
+
+    session.add(condition)
+    session.commit()
