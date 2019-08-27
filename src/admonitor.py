35cd4ed5bc3bb2606e4e2a6dd5ac55722c4d68b0
akakou <s16003@tokyo.kosen-ac.jp>
2019-08-27 13:13:14 +0900

    Webhookではなく、Botを利用するようにした

--- src/admonitor.py
+++ src/admonitor.py
@@ -9,21 +9,26 @@ __version__ = '0.1'
 __date__    = '2018/08/21'
 
 
-import json
+import sys
 import time
+import json
 import textwrap
+import traceback
 from datetime import datetime
 
-import requests
 import feedparser
+from slack import WebClient as SlackClient
 
 import settings
 from model import session, Condition
 
 
+ALERT_CHANNEL = 'alert'
 RSS_URL = 'https://jvndb.jvn.jp/ja/rss/jvndb_new.rdf'
-SLEEP = 3
+SLEEP = 10
 
+
+sc = SlackClient(settings.CLIENT_TOKEN)
 
 class VulnInfo:
     '''脆弱性情報'''
@@ -65,8 +70,8 @@ class VulnInfo:
 class VulnInfoRSS:
     def __init__(self):
         now = time.time()
-        self.last_time = datetime.fromtimestamp(now)
-        # self.last_time = datetime.fromtimestamp(0)
+        # self.last_time = datetime.fromtimestamp(now)
+        self.last_time = datetime.fromtimestamp(0)
         self.last_id = ''
     
     def read_feed(self):
@@ -88,12 +93,8 @@ class VulnInfoRSS:
             if self.last_time <= published_time and self.last_id != entry['id']:
                 self.last_time = published_time
                 self.last_id = entry['id']
-                yield VulnInfo(entry)
+                yield entry
 
-
-def send_webhook(data):
-    '''WEB HOOKを送信する'''
-    requests.post(settings.WEB_HOOK_URL, data=json.dumps(data))
 
 def main():
     '''メイン関数'''
@@ -110,13 +111,22 @@ def main():
         channels = list(set(channels))
 
         for channel in channels:
-            send_webhook({
-                'text': str(vuln),
-                'link_names': 1,
-                'channel': channel
-            })
+            sc.chat_postMessage(
+                channel=channel,
+                text=str(vuln)
+            )
             time.sleep(SLEEP)
 
 
 if __name__=='__main__':
-    main()
+    try:
+       main()
+    except:
+        error = traceback.format_exc()
+
+        sc.chat_postMessage(
+            channel=ALERT_CHANNEL,
+            text=error
+        )
+
+        print(error, file=sys.stderr)
